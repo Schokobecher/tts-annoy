542e55770c87a5d0b0b754f5d8795db0a59ff3b9
Pierre Dennert <pierre@dennert.me>
2017-07-16 22:51:47 +0200

    Code cleanup + Workaround
    Code has been organized and commented. Added a workaround for second
    'undefined' GET request when calling the backend direcly.

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,18 @@
+// express
+var util = require('util');
+var cors = require('cors');
 const express = require('express')
 const app = express()
 const port = 2888
+var validator = require('express-validator');
+app.listen(port)
 
+// middleware
+app.use(validator());
+app.use(cors());
+
+// log4js
 var log4js = require('log4js');
-
 log4js.configure({
     appenders: {
         out: {
@@ -25,65 +34,65 @@ log4js.configure({
 const logger = log4js.getLogger('server');
 
 
-
-const uuid = require('uuid/v4');
-
-var util = require('util');
-var bodyParser = require('body-parser');
-var validator = require('express-validator');
-var cors = require('cors')
-
+// play-sound
 const player = require('play-sound')();
+
+// tts
 var googleTTS = require('google-tts-api');
 
+// misc
 var https = require('https');
 var fs = require('fs');
 const path = require('path')
-
-app.listen(port)
+const uuid = require('uuid/v4');
+// Server started
 logger.info('Server running on ' + port);
-
-// middleware
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
-app.use(validator());
-app.use(cors());
-
 
 
 app.use(function(req, res, next) {
-
+	// Set header options
     res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
 
+	
+	// Request was using GET method
     if (req.method == "GET") {
+		
+		var msg = req.sanitize('message').escape();
+		// TODO: second 'undefined' GET request when calling the backend direcly? We work around it for now...
+		if(undefined == msg) {
+				var msg = '';
+		};
+		if (!msg.length) {
+			return;
+		};
+
         var fileuuid = uuid();
         var file = fs.createWriteStream("./media/" + fileuuid + ".mp3");
-		req.sanitize('message').escape();
-        var msg = req.query.message;
         res.end('Very Nice!')
         logger.info(msg + " via GET received.");
-
+		// Google magic
         googleTTS(msg, 'en', 1) // speed normal = 1 (default), slow = 0.24
             .then(function(url) {
-                logger.info(url);
+               logger.info("Generated: "+ url);
+			   
+			   // Save the response as .mp3
                 var request = https.get(url, function(response) {
                     response.pipe(file);
+					
+					// Play the .mp3
                     player.play("./media/" + fileuuid + ".mp3", (err) => {
                         if (err) logger.fatal(`Could not play sound: ${err}`);
                         logger.info("played file " + fileuuid);
                     });
                 })
             });
-    } else if (req.method == "POST") {
-        res.end("received POST request.");
+    } else if (req.method == "POST") { // POST Request
         logger.info("received POST request.");
 
     } else {
-        res.end("Undefined request.");
-        logger.error("Undefined request.");
+        logger.error("Undefined request."); // Something else...
     }
 
 });
