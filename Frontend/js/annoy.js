62e66d1c88e1381714a52860a218576dfd3926e9
Pierre Dennert <pierre@dennert.me>
2017-07-19 10:36:50 +0200

    Frontend cleanup + Error norification
    Cleaned up the codestyle, added notification when the backend can't be
    reached in time

--- Frontend/js/annoy.js
+++ Frontend/js/annoy.js
@@ -1,28 +1,34 @@
 $(function() {
-  
-  function sendRequest() {
-  	changeButton();
-  	$.get("http://BACKENDURL", {message:$("#messageBox").val()}, function(data){
-  		console.log(data);
-  	});
-  	$("#messageBox").val("");  	
-  }
+	$.ajaxSetup({
+		timeout: 5000,
+		error: function(xhr, status, error) {
+			console.log(error)
+			$("#sendButton").addClass("btn-danger");
+			$("#sendButton").text("There was a backend error");
+		}
+	});
 
-  function changeButton() {
-  	$("#sendButton").text(buttonText[Math.floor(Math.random() * buttonText.length)]);
-  }
-  
-  var buttonText = ["HURT ME", "I CAME", "YEEEES!", "CRASH ME LIKE DIANA", "kek"];
-  changeButton();
+	function sendRequest() {
+		$.get("http://ddns.dennert.me:2888", {
+			message: $("#messageBox").val()
+		}, function(data) {
+			console.log('Success!');
+			changeButton();
+		});
+	}
 
-  $("#sendButton").click(function(){
-  	sendRequest();
-  });
-
-  $("#messageBox").keypress(function (e) {
-  	if(e.which == 13){
-  		sendRequest();
-  	}
-  });
-
+	function changeButton() {
+		$("#sendButton").removeClass("btn-danger");
+		$("#sendButton").text(buttonText[Math.floor(Math.random() * buttonText.length)]);
+	}
+	var buttonText = ["HURT ME", "I CAME", "YEEEES!", "CRASH ME LIKE DIANA", "kek"];
+	changeButton();
+	$("#sendButton").click(function() {
+		sendRequest();
+	});
+	$("#messageBox").keypress(function(e) {
+		if (e.which == 13) {
+			sendRequest();
+		}
+	});
 });
